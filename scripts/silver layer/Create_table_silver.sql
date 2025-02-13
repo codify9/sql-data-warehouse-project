fbd8bd035681f6c8d56023066f452a88696971c6
codify9 <noobcoderhu@gmail.com>
2025-02-14 01:50:39 +0530

    Added a new column

--- scripts/silver layer/Create_table_silver.sql
+++ scripts/silver layer/Create_table_silver.sql
@@ -7,7 +7,8 @@ CREATE TABLE silver.crm_cust_info (
     cst_lastname        VARCHAR(50),
     cst_marital_status  VARCHAR(50),
     cst_gndr            VARCHAR(50),
-    cst_create_date     DATE
+    cst_create_date     DATE,
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Drop and create silver.crm_prd_info
@@ -19,7 +20,8 @@ CREATE TABLE silver.crm_prd_info (
     prd_cost     INTEGER,
     prd_line     VARCHAR(50),
     prd_start_dt TIMESTAMP,
-    prd_end_dt   TIMESTAMP
+    prd_end_dt   TIMESTAMP,
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Drop and create silver.crm_sales_details
@@ -33,14 +35,16 @@ CREATE TABLE silver.crm_sales_details (
     sls_due_dt   INTEGER,
     sls_sales    INTEGER,
     sls_quantity INTEGER,
-    sls_price    INTEGER
+    sls_price    INTEGER,
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Drop and create silver.erp_loc_a101
 DROP TABLE IF EXISTS silver.erp_loc_a101;
 CREATE TABLE silver.erp_loc_a101 (
     cid    VARCHAR(50),
-    cntry  VARCHAR(50)
+    cntry  VARCHAR(50),
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Drop and create silver.erp_cust_az12
@@ -48,7 +52,8 @@ DROP TABLE IF EXISTS silver.erp_cust_az12;
 CREATE TABLE silver.erp_cust_az12 (
     cid    VARCHAR(50),
     bdate  DATE,
-    gen    VARCHAR(50)
+    gen    VARCHAR(50),
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Drop and create silver.erp_px_cat_g1v2
@@ -57,7 +62,8 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
     id           VARCHAR(50),
     cat          VARCHAR(50),
     subcat       VARCHAR(50),
-    maintenance  VARCHAR(50)
+    maintenance  VARCHAR(50),
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
  
