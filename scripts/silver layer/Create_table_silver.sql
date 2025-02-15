983f6783d591ad14960a6ee681b3ee5efcdc6aa6
codify9 <noobcoderhu@gmail.com>
2025-02-15 17:02:47 +0530

    Clean data ingestion in cust_info

--- scripts/silver layer/Create_table_silver.sql
+++ scripts/silver layer/Create_table_silver.sql
@@ -8,7 +8,7 @@ CREATE TABLE silver.crm_cust_info (
     cst_marital_status  VARCHAR(50),
     cst_gndr            VARCHAR(50),
     cst_create_date     DATE,
-    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Drop and create silver.crm_prd_info
@@ -21,7 +21,7 @@ CREATE TABLE silver.crm_prd_info (
     prd_line     VARCHAR(50),
     prd_start_dt TIMESTAMP,
     prd_end_dt   TIMESTAMP,
-    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Drop and create silver.crm_sales_details
@@ -36,7 +36,7 @@ CREATE TABLE silver.crm_sales_details (
     sls_sales    INTEGER,
     sls_quantity INTEGER,
     sls_price    INTEGER,
-    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Drop and create silver.erp_loc_a101
@@ -44,7 +44,7 @@ DROP TABLE IF EXISTS silver.erp_loc_a101;
 CREATE TABLE silver.erp_loc_a101 (
     cid    VARCHAR(50),
     cntry  VARCHAR(50),
-    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Drop and create silver.erp_cust_az12
@@ -53,7 +53,7 @@ CREATE TABLE silver.erp_cust_az12 (
     cid    VARCHAR(50),
     bdate  DATE,
     gen    VARCHAR(50),
-    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Drop and create silver.erp_px_cat_g1v2
@@ -63,7 +63,7 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
     cat          VARCHAR(50),
     subcat       VARCHAR(50),
     maintenance  VARCHAR(50),
-    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
  
