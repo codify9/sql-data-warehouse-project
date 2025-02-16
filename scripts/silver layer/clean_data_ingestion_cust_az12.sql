37dcf4090e3100259bc0bfe8ee3dd13f07be9909
codify9 <noobcoderhu@gmail.com>
2025-02-16 21:47:04 +0530

    Clean data ingestion into cust az12

--- scripts/silver layer/clean_data_ingestion_cust_az12.sql
+++ scripts/silver layer/clean_data_ingestion_cust_az12.sql
@@ -6,10 +6,12 @@ SELECT
         WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4)
         ELSE cid
     END AS cid,
+
     CASE 
         WHEN bdate > CURRENT_DATE THEN NULL
         ELSE bdate
     END AS bdate,
+    
     CASE 
         WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
         WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
